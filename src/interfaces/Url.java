a21325acd614c2be87ced4b7fd94d7d192d4095d
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-10-24 22:46:53 +0200

    Done half of interface exercises

---
+++ src/interfaces/Url.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface Url {
+    public String getParam(String name, String url);
+}
---
+++ src/interfaces/UrlMain.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public class UrlMain {
+    public static void main(String[] args) {
+        StandardUrl standardUrl = new StandardUrl();
+        String param = standardUrl.getParam("param1", "url_example?param1=99&param2=string");
+        System.out.println(param);
+        ExtendedUrl extendedUrl = new ExtendedUrl();
+        String result = extendedUrl.getParam("param2", "url_example/param1.99/param2.string");
+        System.out.println(result);
+    }
+}
