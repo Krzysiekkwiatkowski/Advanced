50cd9b5cbf103f5797420747c2987c9247e9c8a5
Krzysiekkwiatkowski <krzyszt.kwiatkowski@gmail.com>
2018-11-03 20:24:07 +0100

    Last exercise from day 2 homework done

--- src/main/java/homeworkDay2/Main5.java
+++ src/main/java/homeworkDay2/Main5.java
@@ -1,14 +1,24 @@
 package homeworkDay2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class Main5{
+public class Main5 {
     public static void main(String[] args) {
-
+        List<String> list = Arrays.asList("0", "1", "8", "10");
+        Function<String, Integer> convert = (String text) -> Integer.parseInt(text);
+        List<Integer> numbers = changeCollection(list, convert);
+        Integer sum = 0;
+        for (Integer number : numbers) {
+        sum += number;
+        }
+        System.out.println(sum);
     }
 
     static <T> List<T> search(Collection<T> collection, Predicate<T> predicate) {
@@ -18,7 +28,15 @@ public class Main5{
         return list;
     }
 
-    static <T> void consumeCollection(Collection<T> collection, Consumer<T> consumer){
+    static <S, T> List<T> changeCollection(Collection<S> collection, Function<S, T> function){
+        List<T> list = new ArrayList<>();
+        for (S s : collection) {
+            list.add(function.apply(s));
+        }
+        return list;
+    }
+
+    static <T> void consumeCollection(Collection<T> collection, Consumer<T> consumer) {
         for (T t : collection) {
             consumer.accept(t);
         }
